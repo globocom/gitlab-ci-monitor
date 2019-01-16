b2ec59cfaec0054a32fc1299d4b639b469b3a7dd
Michael Descher <michael.descher@1und1.de>
2019-01-16 07:56:45 +0100

    use latest non-skipped build

--- js/app.js
+++ js/app.js
@@ -237,7 +237,7 @@ const app = new Vue({
             return
           }
           for (var i = 0; i < pipelines.data.length; i++) {
-            if (pipelines.data[i].status != 'skipped') { // find latest non-skipped build
+            if (pipelines.data[i].status !== "skipped") { // find latest non-skipped build
               const commitId = pipelines.data[0].sha
               const pipelineId = pipelines.data[0].id
               axios.get('/projects/' + p.data.id + '/repository/commits/' + commitId)
